fdd1b9961593ff437380ffcfb38cecafc030b11e
Simamnkele <smambo85@gmail.com>
2024-05-11 08:48:45 -0700

    Added new endpoint for UsersController

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 // API endpoints
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 /**
- * import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
+ * import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 */
 
@@ -11,9 +11,9 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 /**
- * router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
+ * router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
