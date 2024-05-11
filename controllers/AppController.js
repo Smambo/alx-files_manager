499d768e4c3b81029665842dfa94487eb96f96aa
Simamnkele <smambo85@gmail.com>
2024-05-11 07:01:45 -0700

    Fixed eslint errors

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,4 +1,3 @@
-// eslint-disable import/no-named-as-default
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
@@ -7,13 +6,13 @@ class AppController {
     res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-	});
+    });
   }
 
   static async getStats(req, res) {
     const usersNum = await dbClient.nbUsers();
     const filesNum = await dbClient.nbFiles();
-    res.status(200).json({users: usersNum, files: filesNum});
+    res.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
